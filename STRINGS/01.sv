07417d8a54f153d873b78d5a1595086292ec8a02
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-02 15:19:16 +0530

    Update 01.sv

--- STRINGS/01.sv
+++ STRINGS/01.sv
@@ -1,1 +1,10 @@
-
+module str;
+  string A="VERILOG";
+  string B="SYSTEM VERILOG";
+  string C="UNIVERSAL VERIFICATION METHODOLOGY";
+  
+  initial begin
+    $display("A=%S,B=%S,C=%S",A,B,C);
+    $finish;
+  end
+endmodule
