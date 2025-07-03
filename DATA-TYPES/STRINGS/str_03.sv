10e4fdb286e950220fa6de8ed871a7e89085cb46
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-03 10:58:51 +0530

    Create str_03.sv

---
+++ DATA-TYPES/STRINGS/str_03.sv
@@ -0,0 +1,9 @@
+module str_03;
+  string A=("DESIGN_ENGINEER");
+  initial begin
+    $display("your's output str_03 on A=%s",A.tolower);
+  end
+endmodule
+
+result
+KERNEL: your's output str_03 on A=design_engineer
