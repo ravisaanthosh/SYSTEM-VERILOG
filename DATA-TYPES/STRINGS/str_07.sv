e46b16d78aa00407c97d7945eb55648c1562a9b1
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-03 14:01:05 +0530

    Create str_07.sv

---
+++ DATA-TYPES/STRINGS/str_07.sv
@@ -0,0 +1,13 @@
+module str_07;
+  string A="DESIGN 1245";
+  initial begin
+    $display("your's expected output for str_07 is A=%s",A.itoa());
+  end
+endmodule
+
+result tool error
+work = /home/runner/work/work.lib
+ERROR VCP7292 "String method itoa is not a function." 
+
+expected output..!
+DESIGN
