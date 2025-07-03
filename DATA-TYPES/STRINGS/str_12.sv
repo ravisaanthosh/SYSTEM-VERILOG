b0e0944576b2a915f22fc06fc99fb27c12d53d0a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-03 15:13:41 +0530

    Create str_12.sv

---
+++ DATA-TYPES/STRINGS/str_12.sv
@@ -0,0 +1,14 @@
+module str_12;
+  string A="DESIGN";
+  string B="DESIGN;
+  ";
+  initial begin
+    if(A==B)
+      $display("A and B are equal");
+      else
+        $display("Aand B are not equal");
+  end
+endmodule
+
+result
+# KERNEL: A and B are equal
