9709bf61dcc0510eaaea0f4722f7dfd2e741bd0b
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-03 10:53:47 +0530

    Create str_02.sv

---
+++ DATA-TYPES/STRINGS/str_02.sv
@@ -0,0 +1,9 @@
+module str_02;
+  string A =("design_engineer");
+  initial begin
+  $display("yours output on A=%s ",A.toupper());
+  end
+endmodule
+
+results
+KERNEL: yours output's str_02 on A=DESIGN_ENGINEER
