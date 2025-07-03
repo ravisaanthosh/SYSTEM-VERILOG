708401ea5c5e20ce4e36feb62fdf911d5a69f3d7
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-03 12:39:54 +0530

    Create str_06.sv

---
+++ DATA-TYPES/STRINGS/str_06.sv
@@ -0,0 +1,9 @@
+module str_06;
+  string A=("3455647RING");
+  initial begin
+    $display("your's expected output for str_06 is A=%d",A.atoi());
+  end
+endmodule
+
+result
+KERNEL: your's expected output for str_06 is A=    3455647
