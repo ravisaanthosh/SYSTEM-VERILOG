9fc310e6ee6aee890af08b1f19610cc198d8ff80
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-07 11:00:18 +0530

    Update str_04.sv

--- DATA-TYPES/STRINGS/str_04.sv
+++ DATA-TYPES/STRINGS/str_04.sv
@@ -10,5 +10,15 @@ EDA complier's tool error for put.c keyword
 ERROR VCP7292 "String method putc is not a function." 
 
   EXPECTED OUTPUT 
-   "DESIGD_ENGINNER"
-  
+   "DESIGd_ENGINNER"
+
+update code 
+  module str_04;
+    string A=("DESIGN_ENGINNER");
+  initial begin
+    A.putc(5,"d");
+    $display("your's out str_04 on A=%s",A);
+  end
+endmodule
+  result
+# KERNEL: your's out str_04 on A=DESIGd_ENGINNER
