bf72103759a4ea0d27b74d9ba49978bb2bf90cbc
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-07 15:42:36 +0530

    Update l_20.sv

--- DATA-TYPES/LOGIC /l_20.sv
+++ DATA-TYPES/LOGIC /l_20.sv
@@ -1,1 +1,8 @@
-
+module l_19;
+  logic[4:0]l_data_typ;
+  initial begin
+    $display("your logic data type l_data_typ=%b",l_data_typ);
+    l_data_typ=5'b10101;
+    $display("your logic data type l_data_typ=%b",l_data_typ);
+  end
+endmodule
