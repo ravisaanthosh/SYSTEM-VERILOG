33f555146590159e4bb9ad60d81f1e2c625ef3c0
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-07-07 19:09:41 +0530

    Create l_int31.sv

---
+++ DATA-TYPES/LONG INT/l_int31.sv
@@ -0,0 +1,8 @@
+module long_int31;
+   shortint long_int_31;
+  initial begin
+    $display("long_int_31=%b",long_int_31);
+    long_int_31 =64'b11111111;
+    $display("long_int_31=%b",long_int_31);
+  end
+endmodule
