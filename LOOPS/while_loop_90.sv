ef8d5fe4dbf87ea52e3101c84809bed5056e7319
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-08-05 12:01:16 +0530

    Update while_loop_90.sv

--- LOOPS/while_loop_90.sv
+++ LOOPS/while_loop_90.sv
@@ -8,3 +8,15 @@ module loop_while_90;
   end
   end
 endmodule
+
+your's while_loops_output is=0
+your's while_loops_output is=1
+your's while_loops_output is=2
+your's while_loops_output is=3
+your's while_loops_output is=4
+your's while_loops_output is=5
+your's while_loops_output is=6
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.490 seconds;       Data structure size:   0.0Mb
+Tue Aug  5 02:30:10 2025
