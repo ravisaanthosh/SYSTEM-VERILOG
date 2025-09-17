4e4d3df2e2a00c2e20422085dc584bd537309606
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-09-17 11:03:31 +0530

    Update loc_ex_189.sv

--- ENCAPSULATION/LOCAL/loc_ex_189.sv
+++ ENCAPSULATION/LOCAL/loc_ex_189.sv
@@ -1,1 +1,22 @@
+class loard;
+  local string shivan;
+ local  string parvathi;
+  
+  function preview();
+    shivan="MURUGAN";
+    parvathi="GANESHAN";
+    $display("name of the god's using local keyword is =%0s,=%0s ",shivan,parvathi);
+  endfunction
+endclass
 
+
+module tb;
+  loard l1;
+  initial begin
+    l1=new();
+    l1.preview();
+  end
+endmodule
+
+
+name of the god's using local keyword is =MURUGAN,=GANESHAN 
