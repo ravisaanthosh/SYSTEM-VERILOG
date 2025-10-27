c0ed95255115379fb1a5be6aab67c74a3487ff34
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-10-27 18:03:55 +0530

    Create timing_relationship_259.sv

---
+++ ASSERTIONS/SVA SEQUENCES/timing_relationship_259.sv
@@ -0,0 +1,29 @@
+module timing_releationship;
+  bit clk;
+  bit a,b;
+  
+   always #2 clk = ~clk;
+  
+  sequence s1;
+    a##3b;
+  endsequence
+  
+  property p1;
+    @(posedge clk)s1;
+  endproperty
+  
+  assert property (p1);
+    initial begin
+      $dumpfile("out.vcd");
+      $dumpvars;
+      clk=0;
+      a=0;
+      b=0;
+      #5; a=1;
+      #20; b=1;
+      #50;$finish;
+    end
+    endmodule
+
+
+    
