c149d8f535bec46b91a4571b1519b4f999c9ed44
SANTHOSH R <ravisanthosh0662@gmail.com>
2026-04-08 17:03:12 +0530

    Update using_iff_method_298.sv

--- FUNCTIONAL_COVERAGE/CONDITIONAL_COVERAGE/using_iff_method_298.sv
+++ FUNCTIONAL_COVERAGE/CONDITIONAL_COVERAGE/using_iff_method_298.sv
@@ -56,3 +56,125 @@ endmodule
 # VSIM: Simulation has finished. There are no more test vectors to simulate.
 # ACDB: Covergroup Coverage data has been saved to "fcover.acdb" database.
 # VSIM: Simulation has finished.
+# +++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++       REPORT INFO       ++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++
+# 
+# 
+# SUMMARY
+# ===============================================
+# |     Property     |          Value           |
+# ===============================================
+# | User             | runner                   |
+# | Host             | f53a78ea6718             |
+# | Tool             | Riviera-PRO 2025.04      |
+# | Report file      | /home/runner/cov.txt     |
+# | Report date      | 2026-04-08 07:32         |
+# | Report arguments | -verbose                 |
+# | Input file       | /home/runner/fcover.acdb |
+# | Input file date  | 2026-04-08 07:32         |
+# | Number of tests  | 1                        |
+# ===============================================
+# 
+# 
+# TEST DETAILS
+# ==============================================
+# | Property |              Value              |
+# ==============================================
+# | Test     | fcover.acdb:fcover              |
+# | Status   | Ok                              |
+# | Args     | asim +access+r                  |
+# | Simtime  | 145 ns                          |
+# | Cputime  | 0.149 s                         |
+# | Seed     | 1                               |
+# | Date     | 2026-04-08 07:32                |
+# | User     | runner                          |
+# | Host     | f53a78ea6718                    |
+# | Host os  | Linux64                         |
+# | Tool     | Riviera-PRO 2025.04 (simulator) |
+# ==============================================
+# 
+# 
+# +++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++     DESIGN HIERARCHY    ++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++
+# 
+# 
+# CUMULATIVE SUMMARY
+# =============================================
+# |    Coverage Type    | Weight | Hits/Total |
+# =============================================
+# | Covergroup Coverage |      1 |   100.000% |
+# |---------------------|--------|------------|
+# | Types               |        |      1 / 1 |
+# =============================================
+# CUMULATIVE INSTANCE-BASED COVERAGE: 100.000%
+# COVERED INSTANCES: 1 / 1
+# FILES: 1
+# 
+# 
+# CLASS - /coverage_using_iff : work.coverage_using_iff
+# 
+# 
+#     SUMMARY
+#     =============================================
+#     |    Coverage Type    | Weight | Hits/Total |
+#     =============================================
+#     | Covergroup Coverage |      1 |   100.000% |
+#     |---------------------|--------|------------|
+#     | Types               |        |      1 / 1 |
+#     =============================================
+#     WEIGHTED AVERAGE LOCAL: 100.000%
+# 
+# 
+#     COVERGROUP COVERAGE
+#     ===================================================================
+#     |           Covergroup            |   Hits   |  Goal /  | Status  |
+#     |                                 |          | At Least |         |
+#     ===================================================================
+#     | TYPE /coverage_using_iff/coverr | 100.000% | 100.000% | Covered |
+#     ===================================================================
+# 
+# 
+# +++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++       DESIGN UNITS      ++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++
+# 
+# 
+# CUMULATIVE SUMMARY
+# =============================================
+# |    Coverage Type    | Weight | Hits/Total |
+# =============================================
+# | Covergroup Coverage |      1 |   100.000% |
+# |---------------------|--------|------------|
+# | Types               |        |      1 / 1 |
+# =============================================
+# CUMULATIVE DESIGN-BASED COVERAGE: 100.000%
+# COVERED DESIGN UNITS: 1 / 1
+# FILES: 1
+# 
+# 
+# CLASS - work.coverage_using_iff
+# 
+# 
+#     SUMMARY
+#     =============================================
+#     |    Coverage Type    | Weight | Hits/Total |
+#     =============================================
+#     | Covergroup Coverage |      1 |   100.000% |
+#     |---------------------|--------|------------|
+#     | Types               |        |      1 / 1 |
+#     =============================================
+#     WEIGHTED AVERAGE: 100.000%
+# 
+# 
+#     COVERGROUP COVERAGE
+#     ===================================================================
+#     |           Covergroup            |   Hits   |  Goal /  | Status  |
+#     |                                 |          | At Least |         |
+#     ===================================================================
+#     | TYPE /coverage_using_iff/coverr | 100.000% | 100.000% | Covered |
+#     ===================================================================
+# 
+exit
+# VSIM: Simulation has finished.
