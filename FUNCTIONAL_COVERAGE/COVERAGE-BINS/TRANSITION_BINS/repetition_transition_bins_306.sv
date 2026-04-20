42660cfdae0dc937f28c9073b9c820d58199b246
SANTHOSH R <ravisanthosh0662@gmail.com>
2026-04-20 17:48:43 +0530

    Create repetition_transition_bins_306.sv

---
+++ FUNCTIONAL_COVERAGE/COVERAGE-BINS/TRANSITION_BINS/repetition_transition_bins_306.sv
@@ -0,0 +1,34 @@
+class repetition_transition_bins;
+  rand bit [2:0]a;
+  rand bit [2:0]b;
+  constraint c1{a inside{[0:3]};}
+  constraint c2 {b inside{[3:6]};}
+ 
+  covergroup crg;
+    e1:coverpoint a bins value_1 =(1[*2]);
+    e2:coverpoint b bins value_2 =(4[*3]);
+  endgroup
+  
+    function new();
+    crg=new();
+  endfunction
+  
+
+
+endclass
+
+module tb;
+  repetition_transition_bins ob;
+  
+  initial begin
+    ob=new();
+    repeat(205)begin
+      ob.randomize();
+      ob.crg.sample();
+      $display("type of bin=[%0d],=[%0d],overall=[%0f]",ob.a,ob.b,ob.crg.get_coverage());
+    end
+  end
+endmodule
+
+“Repetition transition bins are used to check whether a value occurs consecutively for a specified number of sampling cycles.”
+tool not support these feature
